cd15d4027f672bdb329ca834a51ed3b9babe173e
paulinecoute <pauline.coute@universite-paris-saclay.fr>
2026-01-15 10:23:18 +0100

    création de forms.py , form pour les templates

---
+++ biolib/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.forms import inlineformset_factory
+from .models import CampaignTemplate, TemplatePart
+
+class CampaignTemplateForm(forms.ModelForm):
+    class Meta:
+        model = CampaignTemplate
+        fields = ['name', 'description', 'enzyme', 'is_public']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'enzyme': forms.Select(attrs={'class': 'form-select'}),
+            'is_public': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+class TemplatePartForm(forms.ModelForm):
+    class Meta:
+        model = TemplatePart
+        fields = ['name', 'type_id', 'order', 'is_mandatory', 'is_separable', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom'}),
+            'type_id': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type'}),
+            'order': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Desc.'}),
+            'is_mandatory': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'is_separable': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+TemplatePartFormSet = inlineformset_factory(
+    CampaignTemplate,
+    TemplatePart,
+    form=TemplatePartForm,
+    extra=1,
+    can_delete=True
+)
