b23ca95c3ee550c96a3dc98cdd321991dbeaf9c1
paulinecoute <pauline.coute@universite-paris-saclay.fr>
2026-01-24 21:23:06 +0100

    modif dans urls.py pour sup page intermédiaire

--- my_insillyclo/urls.py
+++ my_insillyclo/urls.py
@@ -9,17 +9,17 @@ urlpatterns = [
 
     # pages principales
     path('', views.home, name='home'),
+    
+    # menu templates
     path('template/', views.template, name='template'),
+    
     path('simulation/', views.simulation, name='simulation'),
 
     # télécharger doc excel
     path('template/export/<int:template_id>/', views.export_template_excel, name='export_template_excel'),
 
-    # gestion des templates
-    # le menu (choix entre créer ou télécharger)
-    path('create_template/', views.create_template, name='create_template'),
-    # le formulaire de création
-    path('create_template/new/', views.template_create_view, name='template_create_view'),
+    # gestion templates
+    path('create_template/new/', views.create_template, name='create_template'),
 
     # page résultats détaillé
     path('simulation_result/', views.simulation_result, name='simulation_result'),
@@ -40,20 +40,16 @@ urlpatterns = [
 
     # espace personnel
     path('dashboard/', views.dashboard, name='dashboard'),
-    # lancer une simulation
+    
+    # simulations
     path('simulation/new/', views.create_simulation, name='create_simulation'),
     path('simulation/<int:pk>/', views.simulation_result, name='simulation_result'),
     path('simulations/', views.simulation_list, name='simulation_list'),
     path('simulation/demo/', views.simulation_result, name='simulation_demo'),
     path('simulation/<int:pk>/csv/', views.download_simulation_csv, name='download_simulation_csv'),
-    path('simulation/<int:pk>/download_zip/', views.download_simulation_zip, name='download_simulation_zip')
-]
+    path('simulation/<int:pk>/download_zip/', views.download_simulation_zip, name='download_simulation_zip'),
 
-# ==============================================================================
-# ÉQUIPES
-# ==============================================================================
-
-urlpatterns += [
+    #équipes
     path('teams/', views.team_list, name='teams'),
     path('teams/create/', views.team_create, name='team_create'),
     path('teams/<int:team_id>/', views.team_detail, name='team_detail'),
